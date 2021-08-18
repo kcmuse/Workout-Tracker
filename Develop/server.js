f45bf1306443869ba324c724d78230f8910623a8
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-08-18 11:23:02 -0700

    server.js created

---
+++ Develop/server.js
@@ -0,0 +1,28 @@
+const routes = require("./routes");
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use("/", routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
